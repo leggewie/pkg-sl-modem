5839bfea4ffbdf7e71f1b4bec84d978755b217d1
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@sabily.org>
2010-06-16 17:52:47 +0300

    Fixed indentation in apport hook

--- debian/source_sl-modem.py
+++ debian/source_sl-modem.py
@@ -2,11 +2,11 @@
 def add_info(report):
   attach_conffiles(report, 'sl-modem-daemon')
   attach_alsa(report)
-	attach_related_packages(report, [
-		"sl-modem-source",
-		] )
+  attach_related_packages(report, [
+    "sl-modem-source"
+    ])
   report['Lsmod'] = command_output(['lsmod'])
   attach_file_if_exists(report, "/var/log/slmodemd/slmodem.log")
   report['Lspci'] = command_output(['lspci','-vvnn'])
   report['Lsusb'] = command_output(['lsusb'])
-	attach_file(report, '/proc/version_signature', 'ProcVersionSignature')
+  attach_file(report, '/proc/version_signature', 'ProcVersionSignature')
