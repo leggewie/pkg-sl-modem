6160610a87e88e23d35c7c364b18de1212faa191
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@sabily.org>
2010-07-10 14:28:32 +0300

    Fixes in apport hook

--- debian/source_sl-modem.py
+++ debian/source_sl-modem.py
@@ -1,38 +1,37 @@
 
 def add_info(report, ui):
+  tags = []
+  WvDialConf = None
   Dialup = ui.yesno("Is there a problem with dialup ?")
-  if Dialup == None: #user cancelled
-    raise StopIteration
   Fax = ui.yesno("Is there a problem with fax ?")
-  if Fax == None: #user cancelled
-    raise StopIteration
 
   if Fax == True:
     tags.append("fax")
-    SLModemdDevice = command_output(['sh', '-c', '[ ! -r /etc/default/sl-modem-daemon ] || (. /etc/default/sl-modem-daemon ; echo $SLMODEMD_DEVICE)'])
-    SLModemdOpts = command_output(['sh', '-c', '[ ! -r /etc/default/sl-modem-daemon ] || (. /etc/default/sl-modem-daemon ; echo $OPTS)'])
-    if ':' in SLModemdDevice or 'alsa' in SLModemdOpts:
+    SLModemdDevice = command_output(['[ ! -r /etc/default/sl-modem-daemon ] || (. /etc/default/sl-modem-daemon ; echo $SLMODEMD_DEVICE)'])
+    SLModemdOpts = command_output(['[ ! -r /etc/default/sl-modem-daemon ] || (. /etc/default/sl-modem-daemon ; echo $OPTS)'])
+    if (':' in SLModemdDevice) or ('alsa' in SLModemdOpts):
       ui.information("sl-modem is configured for ALSA mode, yet fax does not work with ALSA mode")
       # If there isn't a problem with dialup, then abort the bug report:
       if Dialup != True:
         raise StopIteration
 
     lsmod_out = command_output(['lsmod'])
-    if not 'slusb' in lsmod_out and not 'slamr' in lsmod_out:
+    if not 'slusb' in lsmod_out and not 'slamr' in lsmod_out and SLModemdDevice == 'auto':
       ui.information("SLMODEM_DEVICE is set to auto, and neither slamr nor slusb is loaded, please explicitly set SLMODEM_DEVICE to slamr ( or slusb if you are using a USB modem), and try again before continuing with this bug report.")
 
 
   if Dialup == True:
     tags.append("dialup")
     response = ui.yesno("It is recommended to test with wvdial before continuing with this bug report. Did you try dialup using wvdial utility ?")
-    if response == None: #user cancelled
-      raise StopIteration
 
     if response == True:
-      WvDialConf = ui.question_file("Please enter the path to wvdial configuration file, by default it is /etc/wvdial.conf, note that this file will be included in the bug report after removing Username and Password fields")
+      WvDialConf = ui.file("Please enter the path to wvdial configuration file, by default it is /etc/wvdial.conf, note that this file will be included in the bug report after removing Username and Password fields")
     if WvDialConf == None: #user cancelled
       if os.path.isfile('/etc/wvdial.conf'):
         WvDialConf = '/etc/wvdial.conf'
+
+  report.setdefault('Tags', '')
+  report['Tags'] += ' ' + ' '.join(tags)
 
   attach_conffiles(report, 'sl-modem-daemon')
   attach_alsa(report)
@@ -50,3 +49,4 @@ def add_info(report, ui):
   if Dialup == True and WvDialConf != None:
     report['WvDialConf'] = command_output(['sed', '-e',
       '/^\s*\(Password\|Username\)/ d', WvDialConf])
+
